48f7889c02180a509d50e2d61325f2e0e07f7228
Tianyi LIANG <tianyi@igem.org>
2024-05-18 00:20:53 +0800

    refactor: code extracting and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
-function stringToSlug(string: string): string {
-  let slug = String(string).toLowerCase();
-  slug = slug.replace(/[^a-z0-9-]/g, "-");
-  slug = slug.replace(/-+/g, "-");
-  // remove dashes at start and end
-  const start = slug.search(/[^-]/); // find index of first non-dash
-  const end = slug.search(/-+$/); // find index of first end dash
-  return slug.substring(start, end === -1 ? undefined : end);
-}
+import { stringToSlug } from "./src/utils";
 
 // https://vitejs.dev/config/
 export default () => {
