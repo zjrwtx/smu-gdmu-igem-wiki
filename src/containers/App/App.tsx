10b4d3a6c2cde82ab46e1e138f53a96c0db205ab
zxa666 <zxadeguge@gmail.com>
2024-09-11 22:43:10 +0800

    better loading animation

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -5,6 +5,8 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { Footer, Header, Navbar, NotFound, Loading } from "../../components";
 import { getPathMapping, stringToSlug } from "../../utils";
 import { useEffect, useState } from "react";
+import axios from "axios";
+// import path from "path";
 
 const App = () => {
   const pathMapping = getPathMapping();
@@ -18,6 +20,8 @@ const App = () => {
   const title =
     currentPath in pathMapping ? pathMapping[currentPath].title : "Not Found";
 
+
+
   useEffect(() => {
     // document.title = `${title || ""} | ${import.meta.env.VITE_TEAM_NAME} - iGEM ${import.meta.env.VITE_TEAM_YEAR}`;
     document.title = `${title}`;
@@ -26,15 +30,42 @@ const App = () => {
   // 加载页面
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // 模拟加载过程
-    setLoading(true);
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // 2秒后隐藏加载页面
+  // 这是定时两秒动画
+  // useEffect(() => {
+  //   // 模拟加载过程
+  //   console.log(`https://${import.meta.env.VITE_TEAM_YEAR}.igem.wiki/${import.meta.env.VITE_TEAM_NAME}${location.pathname}`);
+  //   setLoading(true);
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000); // 2秒后隐藏加载页面
 
-    return () => clearTimeout(timer);
-  }, [location.pathname]); // 监听路由变化
+  //   return () => clearTimeout(timer);
+  // }, [location.pathname]); // 监听路由变化
+
+
+
+  // // 这是加载完成后0.5秒结束播放动画
+  useEffect(() => {
+    // 发起网络请求
+    console.log(`https://${import.meta.env.VITE_TEAM_YEAR}.igem.wiki/${import.meta.env.VITE_TEAM_NAME}${location.pathname}`);
+    axios.get(`https://${import.meta.env.VITE_TEAM_YEAR}.igem.wiki/${import.meta.env.VITE_TEAM_NAME}${location.pathname}`)
+    
+      .then(response => {
+        // 处理响应数据
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+      .finally(() => {
+        setLoading(true);
+        // 请求完成后隐藏加载动画
+        setTimeout(() => {
+        setLoading(false);
+      },500);
+
+      });
+  }, [location.pathname]);
 
   return (
     <>
