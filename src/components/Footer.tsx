d4e8ec79ebf48eb016b7159466787433f1663fe3
zxa666 <zxadeguge@gmail.com>
2024-09-12 16:34:07 +0800

    new footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,37 +6,33 @@ export function Footer() {
   const teamSlug = stringToSlug(teamName);
 
   return (
-    <footer className="pt-5 pb-5 footer py-5 mt-0 bg-dark text-white">
-      <div className="container">
-        <div className="row mb-4">
-            <div className="col-lg-2 col-xs-12">
-              <img
-                src="https://static.igem.wiki/teams/5378/school-badge/smu.webp"
-                alt="SMU brand"
-                className="responsive-img"
-              />
-            </div>
-            <div className="col-lg-2"></div>
-            <div className="col-lg-2 col-xs-12">
-              <img
-                src="https://static.igem.wiki/teams/5378/school-badge/gdmu.webp"
-                alt="GDMU brand"
-                className="responsive-img"
-              />
-            </div>
-            <div className="col-lg-2"></div>
-          <div className="col-lg-4 col-xs-12 row justify-content-center">
-          <img
+    <footer className="pt-1 pb-1 footer py-5 mt-0 bg-wine-red text-white">
+        <hr />
+        {/* The following MUST be on every page: license information and link to the repository on gitlab.igem.org */}
+        <div className="row">
+          <div className="col-4">
+
+            {/* <div className="col-1"></div> */}
+            {/* <div className="col-5"> */}
+              <div className="slideshow-container">
+                      <div className="slide fade1 ">
+                        <img className="fade-img" src="https://static.igem.wiki/teams/5378/school-badge/gdmu.webp" alt="Image 1" />
+                      </div>
+                      <div className="slide fade2">
+                        <img className="fade-img" src="https://static.igem.wiki/teams/5378/school-badge/smu.webp" alt="Image 2" />
+                      </div>
+                      <div className="slide fade3">
+                        <img className="fade-img" src="https://static.igem.wiki/teams/5378/school-badge/yanyintech.webp" alt="Image 3" />
+                    </div>
+              </div>
+          {/* <img
                 src="https://static.igem.wiki/teams/5378/school-badge/yanyintech.webp"
                 alt="Yanyin tech"
                 className="responsive-img"
-              />
+              /> */}
+
           </div>
-        </div>
-        <hr />
-        {/* The following MUST be on every page: license information and link to the repository on gitlab.igem.org */}
-        <div className="row mt-4">
-          <div className="col">
+          <div className="col-8">
             <p className="mb-0">
               <small>
                 © 2024 - Content on this site is licensed under a{" "}
@@ -61,7 +57,6 @@ export function Footer() {
             </p>
           </div>
         </div>
-      </div>
     </footer>
   );
 }
