fc6142fc9a19082dd3694cd43cc9f375ce1485ab
Kang Wang <888666wang4286@gmail.com>
2024-09-22 15:29:41 +0000

    Update file model.tsx

--- src/contents/model.tsx
+++ src/contents/model.tsx
@@ -67,15 +67,15 @@ export function Model() {
               <h2 className="center-text">Section 1  ODE Model of Biochemical Reactions</h2>
                  <h3>1.1 Oxidation of Phenylethylamine</h3>
                   <p>Firstly, phenylethylamine (PEA) diffuses through the outer membrane of <em>Escherichia coli</em> into the periplasmic space, where it interacts with TynA.</p>
-              <MathJax.Provider>
+              <MathJax.Provider className="mathjax-provider">
                   <div className='indent formula_content' >
                       <MathJax.Node  formula={`{PEA_{gut}\\overset{k_{\\text{diff}}\\_{\\text{PEA}}}{\\underset{k_{\\text{diff}}\\_{\\text{PEA}}}{\\rightleftharpoons}}  PEA_{peri}}`} />
                       <span className='formula_number'>1</span>
-                  
+
                   </div>
               </MathJax.Provider>
               <p>According to the law of mass action, this process can be represented by an ordinary differential equation (ODE) as follows</p>
-              <MathJax.Provider>
+              <MathJax.Provider className="mathjax-provider">
                   <div className='indent formula_content' >
                       <MathJax.Node formula={`\\frac{\\mathrm{d}[PEA_{peri}]}{\\mathrm{d}t} = \\frac{k_{diff\\_PEA}}{V_{peri}}([PEA_{gut}] - [PEA_{peri}])`} />
                       <span className='formula_number'>2</span>
