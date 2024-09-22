126e9b8bf17df2a17324b9e707cdb19347464bac
zjrwtx <3038880699@qq.com>
2024-09-22 01:30:58 +0000

    all

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,72 +1,3 @@
-// import Nav from "react-bootstrap/Nav";
-// import BootstrapNavbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-// import { Link } from "react-router-dom";
-// import Pages from "../pages.ts";
-// import { Container } from "react-bootstrap";
-
-
-// export function Navbar() {
-//   const pages = Pages.map((item, pageIndex) => {
-//     if ("folder" in item && item.folder) {
-//       const folderItems = item.folder.map((subpage, subpageIndex) => {
-//         if (subpage.path) {
-//           return (
-//             <NavDropdown.Item
-//               key={`subpage-${pageIndex}-${subpageIndex}`}
-//               as={Link}
-//               to={subpage.path}
-//               className="custom-dropdown-menu"
-//             >
-//               {subpage.name}
-//             </NavDropdown.Item>
-//           );
-//         }
-//       });
-//       return (
-//         <NavDropdown
-//           key={`page-${pageIndex}`}
-//           title={item.name}
-//           id="basic-nav-dropdown"
-//           className="custom-dropdown-menu"
-//         >
-//           {folderItems}
-//         </NavDropdown>
-//       );
-//     } else if ("path" in item && item.path) {
-//       return (
-//         <Nav.Link 
-//         key={`page-${pageIndex}`} as={Link} to={item.path}
-//         className="custom-dropdown-menu"
-//         >
-//           {item.name}
-//         </Nav.Link>
-//       );
-//     }
-//   });
-
-//   return (
-
-//     // 这部分是顶部展开navBar
-//     <BootstrapNavbar expand="lg" className="bg-body-tertiary" fixed="top">
-//       <Container>
-//         <BootstrapNavbar.Brand className="left-aligned">
-//           {/* {import.meta.env.VITE_TEAM_NAME} */}
-//           <img src="https://static.igem.wiki/teams/5378/lesser-panda/logo.webp" className="navbar-img" />
-//         </BootstrapNavbar.Brand>
-//         <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
-//         <BootstrapNavbar.Collapse id="basic-navbar-nav">
-//           <Nav className="custom-nav-item">{pages}</Nav>
-//         </BootstrapNavbar.Collapse>
-//       </Container>
-//     </BootstrapNavbar>
-
-
-//   );
-// }
-
-
-
 
 import Nav from "react-bootstrap/Nav";
 import BootstrapNavbar from "react-bootstrap/Navbar";
@@ -116,14 +47,14 @@ export function Navbar() {
   });
 
   return (
-    <BootstrapNavbar expand="lg" className="bg-body-tertiary custom-navbar" fixed="top">
-      <Container>
-        <BootstrapNavbar.Brand className="left-aligned">
-          <img src="https://static.igem.wiki/teams/5378/lesser-panda/logo.webp" className="navbar-img" alt="Logo" />
+    <BootstrapNavbar expand="lg" className="apple-navbar" fixed="top">
+      <Container fluid>
+        <BootstrapNavbar.Brand as={Link} to="/" className="apple-brand">
+          <img src="https://static.igem.wiki/teams/5378/lesser-panda/logo.webp" className="apple-logo" alt="Logo" />
         </BootstrapNavbar.Brand>
-        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" className="apple-toggler" />
         <BootstrapNavbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto custom-nav">{pages}</Nav>
+          <Nav className="ms-auto apple-nav">{pages}</Nav>
         </BootstrapNavbar.Collapse>
       </Container>
     </BootstrapNavbar>
