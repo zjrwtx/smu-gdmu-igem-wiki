102b19a53eb00f5e79ef07182e04b4c6088f9641
zxa666 <zxadeguge@gmail.com>
2024-09-23 20:43:05 +0800

    more team member card

--- src/contents/team.tsx
+++ src/contents/team.tsx
@@ -56,7 +56,7 @@ const StickyImageContainer: React.FC<{ image: string ,words: string}> = ({ image
 
 // 下方定义重要！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
 export function Team() {
-  const [hoveredCardId, setHoveredCardId] = useState<"card1" | "card2" | "card3" | "card4" | null>(null);
+  const [hoveredCardId, setHoveredCardId] = useState<"card1" | "card2" | "card3" | "card4" | "card5" | "card6" | "card7" | "card8" | "card9" | "card10" | "card11" | "card12" | "card13" | "card14" | "card15" | "card16" | "card17" | "card18" | "card19" | "card20" | "card21" | "card22" | "card23" | "card24" | "card25" | "card26" | "card27" | "card28" | "card29" | "card30" | "card31" | "card32" | "card33" | "card34" | "card35" | null>(null);
 
 
   // 定义每个卡片的图像，当悬浮时左侧显示相应的图像
@@ -65,20 +65,85 @@ export function Team() {
     card1: 'https://static.igem.wiki/teams/5378/school-badge/smu.webp',
     card2: 'https://static.igem.wiki/teams/5378/school-badge/gdmu.webp',
     card3: 'https://static.igem.wiki/teams/5378/image/zxa-tp.webp',
-    card4: 'https://static.igem.wiki/teams/5378/safety/03-111.webp'
+    card4: 'https://static.igem.wiki/teams/5378/safety/03-111.webp',
+    card5: 'https://placehold.co/600x800',
+    card6: 'https://placehold.co/600x800',
+    card7: 'https://placehold.co/600x800',
+    card8: 'https://placehold.co/600x800',
+    card9: 'https://placehold.co/600x800',
+    card10: 'https://placehold.co/600x800',
+    card11: 'https://placehold.co/600x800',
+    card12: 'https://placehold.co/600x800',
+    card13: 'https://placehold.co/600x800',
+    card14: 'https://placehold.co/600x800',
+    card15: 'https://placehold.co/600x800',
+    card16: 'https://placehold.co/600x800',
+    card17: 'https://placehold.co/600x800',
+    card18: 'https://placehold.co/600x800',
+    card19: 'https://placehold.co/600x800',
+    card20: 'https://placehold.co/600x800',
+    card21: 'https://placehold.co/600x800',
+    card22: 'https://placehold.co/600x800',
+    card23: 'https://placehold.co/600x800',
+    card24: 'https://placehold.co/600x800',
+    card25: 'https://placehold.co/600x800',
+    card26: 'https://placehold.co/600x800',
+    card27: 'https://placehold.co/600x800',
+    card28: 'https://placehold.co/600x800',
+    card29: 'https://placehold.co/600x800',
+    card30: 'https://placehold.co/600x800',
+    card31: 'https://placehold.co/600x800',
+    card32: 'https://placehold.co/600x800',
+    card33: 'https://placehold.co/600x800',
+    card34: 'https://placehold.co/600x800',
+    card35: 'https://placehold.co/600x800',
+
+
   };
 
   const words = {
     card1: '大家好啊，我是zxa，asdasdsadasdsasdadwdawdawdasdawdawsdaw附件格式艰苦虎骨酒扣税的哎u了如何改进南方灯具开关哎u人身攻击喀什地方韩国u爱我人员孤独和干部埃卢瑞干哈及时答复客户给',
     card2: '我是wyf，地地道道的地地道道的地对地导弹顶顶顶顶',
     card3: '我是leeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-    card4: '芜湖'
+    card4: '',
+    card5: '',
+    card6: '',
+    card7: '',
+    card8: '',
+    card9: '',
+    card10: '',
+    card11: '',
+    card12: '',
+    card13: '',
+    card14: '',
+    card15: '',
+    card16: '',
+    card17: '',
+    card18: '',
+    card19: '',
+    card20: '',
+    card21: '',
+    card22: '',
+    card23: '',
+    card24: '',
+    card25: '',
+    card26: '',
+    card27: '',
+    card28: '',
+    card29: '',
+    card30: '',
+    card31: '',
+    card32: '',
+    card33: '',
+    card34: '',
+    card35: '',
+
   };
 
   // 下方定义重要！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
   const handleCardHover = (id: string, isHovered: boolean) => {
     if (isHovered) {
-      setHoveredCardId(id as "card1" | "card2" | "card3" | "card4");
+      setHoveredCardId(id as "card1" | "card2" | "card3" | "card4" | "card5" | "card6" | "card7" | "card8" | "card9" | "card10" | "card11" | "card12" | "card13" | "card14" | "card15" | "card16" | "card17" | "card18" | "card19" | "card20" | "card21" | "card22" | "card23" | "card24" | "card25" | "card26" | "card27" | "card28" | "card29" | "card30" | "card31" | "card32" | "card33" | "card34" | "card35");
     } else {
       setHoveredCardId(null);
     }
