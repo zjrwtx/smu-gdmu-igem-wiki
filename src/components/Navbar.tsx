49d543f083627237f37141f6a0a9f0d548e6c85a
zjrwtx <3038880699@qq.com>
2024-09-26 00:37:37 +0800

    test the image navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import Pages from "../pages";
 import { Container } from "react-bootstrap";
 import "./Navbar.css";
+// test
 
 export function Navbar() {
   const renderNavItem = (item: any, pageIndex: number, isSubItem: boolean = false) => {
