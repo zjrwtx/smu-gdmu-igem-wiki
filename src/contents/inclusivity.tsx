c7d754115bafc4158f1bfeb613ee814e63091692
Xingan Zhao <2081098605@qq.com>
2024-09-29 22:25:31 +0800

    feat:inclusivity

--- src/contents/inclusivity.tsx
+++ src/contents/inclusivity.tsx
@@ -12,10 +12,10 @@ const SideNavbar: React.FC<SideNavbarProps> = ({ activeLink }) => {
       <Nav className="flex-column">
         <Nav.Link as={Link} to="section1" smooth={true} duration={500} className={activeLink === 'section1' ? 'active' : 'notActive'}>Section 1</Nav.Link>
         <Nav.Link as={Link} to="section2" smooth={true} duration={500} className={activeLink === 'section2' ? 'active' : 'notActive'}>Section 2</Nav.Link>
-        <Nav.Link as={Link} to="section3" smooth={true} duration={500} className={activeLink === 'section3' ? 'active' : 'notActive'}>Section 3</Nav.Link>
-        <Nav.Link as={Link} to="section4" smooth={true} duration={500} className={activeLink === 'section4' ? 'active' : 'notActive'}>Section 4</Nav.Link>
-        <Nav.Link as={Link} to="section5" smooth={true} duration={500} className={activeLink === 'section5' ? 'active' : 'notActive'}>Section 5</Nav.Link>
-        <Nav.Link as={Link} to="section6" smooth={true} duration={500} className={activeLink === 'section6' ? 'active' : 'notActive'}>Section 6</Nav.Link>
+        <Nav.Link as={Link} to="section3" smooth={true} duration={500} className={activeLink === 'section3' ? 'active' : 'notActive'}>Overview</Nav.Link>
+        <Nav.Link as={Link} to="section4" smooth={true} duration={500} className={activeLink === 'section4' ? 'active' : 'notActive'}>Rural outreach program</Nav.Link>
+        <Nav.Link as={Link} to="section5" smooth={true} duration={500} className={activeLink === 'section5' ? 'active' : 'notActive'}>The arts</Nav.Link>
+        <Nav.Link as={Link} to="section6" smooth={true} duration={500} className={activeLink === 'section6' ? 'active' : 'notActive'}>Link with the world</Nav.Link>
         <Nav.Link as={Link} to="section7" smooth={true} duration={500} className={activeLink === 'section7' ? 'active' : 'notActive'}>Section 7</Nav.Link>
         <Nav.Link as={Link} to="section8" smooth={true} duration={500} className={activeLink === 'section8' ? 'active' : 'notActive'}>Section 8</Nav.Link>
         <Nav.Link as={Link} to="section9" smooth={true} duration={500} className={activeLink === 'section9' ? 'active' : 'notActive'}>Section 9</Nav.Link>
@@ -107,6 +107,14 @@ export function Inclusivity() {
               <h4 className="center-text">To see</h4>
               <p className="indent">Through iGEM, we recognize the importance of diversity and inclusivity in the scientific community. We are committed to making the synthetic biology community more accessible to minority groups and creating a learning environment where everyone feels welcome. Our project focuses on the urban-rural divide, aiming to help rural residents break down barriers to information and promote educational equity. At the same time, we have adopted a completely new approach by combining synthetic biology with Socially popular art crafts, allowing everyone to benefit from our education. In our project, we have consulted experts in various fields and communicated with other iGEM teams. We are convinced that through these initiatives, we are connected to the entire world.</p>
               </div>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+              </div>
               </Element>
 
               <Element name="section4" className="element rounded-border" id='section4'>
@@ -115,23 +123,62 @@ export function Inclusivity() {
               <div className="rounded-border">
               <h4 className="center-text">First Encounter and Gifts</h4>
               <p className="indent">On July 14, 2024, the SMU-GDMU iGEM team, in collaboration with a doctoral medical team from Southern Medical University, embarked on a journey to Wa Ri Town, Daofu County, Garze Tibetan Autonomous Prefecture, Sichuan Province, located at an altitude of 3000 meters. Daofu County is nestled along the Xianshui River, where the river's relentless erosion over the years has created a series of fertile valleys, giving birth to a constellation of villages. The iGEM team's visit brought medical supplies and equipment to the villagers of Wa Ri Town for their daily healthcare services.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/1-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+              </div>
               </div>
 
               <div className="rounded-border">
               <h4 className="center-text">Research and Advocacy</h4>
               <p className="indent">The iGEM team collaborated with local health clinics to conduct in-depth interviews and focus group discussions. Through accurate data collection, the team gained a comprehensive understanding of the recent trends in the incidence of hepatic encephalopathy and the current state of treatment in the area. Subsequently, we provided educational sessions on synthetic biology for the local villagers and held a popular science lecture on "hepatic encephalopathy" for them. To gauge the villagers' understanding and feedback on the knowledge of "hepatic encephalopathy" we shared, we visited villagers' homes for follow-up, hoping to further promote their awareness of the condition.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/20240922214356-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/44-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+              </div>
               </div>
               
               <div className="rounded-border">
               <h4 className="center-text">Free Medical Services</h4>
               <p className="indent">Not only that, but to better serve the villagers, the iGEM team, in collaboration with town government and health clinic staff, also provided comprehensive free medical services to the left-behind villagers by going deep into the villages. While waiting for the free medical services, Teacher Li Ling, the guidance teacher of the iGEM team, and other team members conducted a detailed survey among the villagers. The aim was to understand their awareness of the prevention and treatment of hepatic encephalopathy, as well as the treatment experiences and living habits of the patients.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/555555-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                </div>
               <p className="indent">At the same time, the team also had in-depth exchanges with the local health clinic, collected detailed information about hepatic encephalopathy, and compiled a list of villagers. Starting with the patients who needed the most attention, team members conducted one-on-one visits, communication, and health guidance in each household. We hope that through this initiative, we can help them understand and prevent hepatic encephalopathy, and eliminate the information barriers associated with living in rural areas.</p>
               </div>
               
               <div className="rounded-border">
               <h4 className="center-text">Health Records</h4>
               <p className="indent">Furthermore, the iGEM team also assisted the doctors at the local health clinic in improving the health records of the villagers, providing more meticulous care and support for the health of the local residents. This series of activities not only enhanced the villagers' understanding of hepatic encephalopathy but also made a positive contribution to the improvement of public health standards in the area.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/666666666-1.webp"
+                alt="example"
+                className="image-height"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                </div>
               </div>
+              
               
               </Element>
 
@@ -142,7 +189,15 @@ export function Inclusivity() {
               <h4 className="center-text">Charity Sale Event</h4>
               <p className="indent">Our team members participated in a charity sale event at Jiayu Sun City Square, Baiyun District, Guangzhou City, Guangdong Province. We integrated elements of synthetic biology and hepatic encephalopathy into our squishy toy creations, printing related patterns and scientific knowledge on them. We hope that through this creation, we can combine art with science, retaining fun, ornamental value, and educational significance.</p>
               </div>
-             
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/77777-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                </div>
+              
               <div className="rounded-border">
               <h4 className="center-text">Community Service Station</h4>
               <p className="indent">In addition, our team also went to the community service station to attract the public with our handicrafts—"squishy toys"—and promote knowledge about synthetic biology and hepatic encephalopathy to them. Subsequently, we issued a survey questionnaire to receive feedback from the public, and the results showed that we indeed played an educational role for the general public."</p>
@@ -158,6 +213,15 @@ export function Inclusivity() {
               <p className="indent">Modern science is both highly differentiated and highly integrated, while intersectional science combines differentiation and synthesis to realize the integration of science. Interdisciplinary  science is a comprehensive and interdisciplinary product, which is conducive to solving the major complex scientific problems, social problems and global problems faced by mankind.</p>
               <p className="indent">Our team is composed of students from various disciplines, including Clinical Medicine, Statistics, Bioinformatics, Psychiatry, Preventive Medicine, Medical Laboratory Technology, Biomedical Materials Science, Applied Psychology, Information Management and Information Systems, and Health Insurance Studies, etc.. This diversity contributes to inspiring thinking collisions and continuous innovation.</p>
               <p className="indent">"Alone we can do so little; together we can do so much."Team building is highly valued, and we owe our success and harmonious atmosphere to it. When we build connections with each other, when we realize our responsibility for the project, when we feel a sense of belonging to the team, it's natural to dedicate to the research. We hope all future iGEM teams attach importance to team building, which undoubtedly contributes to work efficiency and most importantly, precious relationships.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/888888888-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                </div>
+              
               </div>
               
               <div className="rounded-border">
@@ -165,6 +229,23 @@ export function Inclusivity() {
               <p className="indent">The team collaborated with the Liver Disease Center of Southern Hospital and the Microecological Diagnosis and Treatment Center of Pearl River Hospital. Under their guidance, the team collected the serum of cirrhosis patients before TIPS operation and conducted metabolomics detection. They also followed up on the patients for three months to record the incidence of HE;</p>
               <p className="indent">The team participated in the South China Exchange Conference, sharing the progress of the project and project experience with Shenzhen University, Southern University of Science and Technology, etc.;</p>
               <p className="indent">The team was invited to participate in the China Regional Exchange Conference CCIC, where they had in-depth communication with other teams to improve project optimization and cooperation, promoting the development of their respective projects.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/1010101001-4-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+
+                <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/12121212-1-1-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                </div>
+              
+              
               </div>
 
               <div className="rounded-border">
@@ -175,6 +256,23 @@ export function Inclusivity() {
               <p className="indent">To resolve experimental issues, we consulted with Professor Chen from the School of Basic Medical Sciences at Southern Medical University and also had an online discussion with Teacher Qiu from the National University of Defense Technology.</p>
               <p className="indent">Additionally, another female scientist we interviewed was Professor Ren from the School of Marxism, who provided us with advice from an ethical safety perspective. She advised that any genetic editing of organisms must be conducted in accordance with existing ethical regulations, especially adhering to the "do not disseminate principle"; at the same time, it is imperative not to release genetically modified organisms or products of genetically modified organisms from the laboratory into the environment, and any sites of genetic editing or deletion and the sequences edited must be reported to the safety committee.</p>
               <p className="indent">Subsequently, she reminded us that our project should be educational and popularizing, capable of enhancing the public's understanding and awareness of synthetic biology, and through public interaction and the organization of science popularization activities, allow more people to understand the significance, value, and application prospects of the project.</p>
+              <div className='image-container'>
+              <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/999999999-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+
+                <img 
+                src="https://static.igem.wiki/teams/5378/award-inclusivity/13131313-2-1.webp"
+                alt="example"
+                className="image-wide"
+                />
+                <figcaption className='caption'>12345678</figcaption>
+                </div>
+              
+              
               </div>
 
               </Element>
