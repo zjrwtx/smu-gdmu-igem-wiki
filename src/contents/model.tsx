fd4ceb74876c93a2a9e679bb54955c78c1a24ce0
Kang Wang <888666wang4286@gmail.com>
2024-09-30 14:00:26 +0000

    model-表格修改

--- src/contents/model.tsx
+++ src/contents/model.tsx
@@ -1018,7 +1018,7 @@ export function Model() {
                             </div>
                         </div>
                         <p>To identify knockout targets, we ran FBA optimization on iDK1463, yielding the flux values for various metabolic reactions in the optimized strain. Next, we filtered out the reactions related to ammonia, excluding those with a flux of zero under normal physiological conditions. The biomass reaction and reactions linearly related to it were considered essential and not selected as targets. Ultimately, among the **6 reactions identified**, the flux of ammonia-producing reactions will be minimized, while the flux of ammonia-consuming reactions will be maximized.</p>
-                        <table className="three-line-table">
+                        <table className="model-table three-line-table">
                             <thead>
                                 <tr className='table-head-line'>
                                     <th>ID</th>
@@ -1071,7 +1071,7 @@ export function Model() {
                             </div>
                         </div>
                         <p>Based on the OptiGene algorithm, we identified two valuable target reactions: <b>Glycine Cleavage System (GLYCL)</b> and <b>Glutamate Dehydrogenase (NADP, GLUDy)</b>. The main reactions involved, the candidate knockout targets, and the effects of their knockout on flux are summarized in the table below:</p>
-                        <table className="three-line-table">
+                        <table className="model-table three-line-table">
                             <thead>
                                 <tr className='table-head-line'>
                                     <th>Target Reaction</th>
@@ -1096,7 +1096,7 @@ export function Model() {
 
                         <p>Subsequently, we evaluated the potential impact of <b>15 candidate genes</b> resulting in <b>32,767 knockout combinations</b> on the growth and ammonia production of the strain using FBA. The results indicated that enhancing the reverse reaction of GLUDy is a key factor in reducing ammonia production, while the knockout of the <b>ECOLIN_RS15500</b> gene (corresponding to the ENO reaction) is a crucial step toward achieving this goal. Although the knockout of the GLYCL reaction can also reduce ammonia production to a small extent, it is not a primary factor. Additionally, different knockout schemes significantly affect the strain's growth, specifically the biomass flux.</p>
                         <p>Considering that excessive knockouts may impair the strain's normal physiological functions and increase operational complexity, we ultimately identified <b>6 alternative knockout schemes</b> that balance ammonia production and biomass:</p>
-                        <table className="three-line-table">
+                        <table className="model-table three-line-table">
                             <thead>
                                 <tr className='table-head-line'>
                                     <th>Target Genes for Knockout</th>
