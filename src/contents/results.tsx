f62ee1f64e05a36f27210d40a55df89cc5435629
Wenyi Guan <2075548612@qq.com>
2024-10-02 00:16:11 +0000

    Update file results.tsx

--- src/contents/results.tsx
+++ src/contents/results.tsx
@@ -96,7 +96,7 @@ export function Results() {
                 <h3>Ammonia degrading ability of GS enzyme</h3>
                 <p>Content for section 2.</p>
                 <img 
-                src="https://static.igem.wiki/teams/5378/school-badge/yanyintech.webp"
+                src="https://static.igem.wiki/teams/5378/result-fig2.webp"
                 alt="example"
                 className="responsive-img"
               />
@@ -108,7 +108,7 @@ export function Results() {
                 <p>As shown in Figure 2b, the difference between EcN_vector and EcN_GS with lower NH4Cl concentration such as 5μM decreased and was even not significant with 0.5μM. This indicates that the GS enzyme requires a rather high level NH4Cl to reach the most efficient status. Therefore, when the level of ammonia is in a normal range, the metabolic module is not likely to cause a significant decrease in ammonia, which might be useful in other metabolic cycles. </p>
                 <p>These results could allay some safety concerns of expressing GS enzymes to some extent, but more animal experiments are essential for further evaluation.</p>
                 <img 
-                src="https://static.igem.wiki/teams/5378/result-fig2.webp"
+                src=""
                 alt="result"
                 className="responsive-img"
               />
